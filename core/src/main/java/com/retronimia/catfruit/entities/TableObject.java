d18cf90ea09a204e5953db5863af9956807687c0
SoHaiNp <139929891+SoHaiNp@users.noreply.github.com>
2025-03-09 06:26:17 -0300

    Objetos agora são criados em cima da mesa

---
+++ core/src/main/java/com/retronimia/catfruit/entities/TableObject.java
@@ -0,0 +1,60 @@
+package com.retronimia.catfruit.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public class TableObject {
+
+    public enum Type {
+        MILK_BOX, JUICE_GLASS, EMPTY_GLASS
+    }
+
+    private Texture texture;
+    private float x, y;
+    private Rectangle bounds;
+    public static final float WIDTH = 128;
+    public static final float HEIGHT = 128;
+    private static final float OBJECT_Y_POSITION = 135; // Posição fixa na mesa
+
+    public TableObject(float x, Type type) {
+        this.x = x;
+        this.y = OBJECT_Y_POSITION;
+
+        switch (type) {
+            case MILK_BOX:
+                this.texture = new Texture(Gdx.files.internal("caixa_de_leite.png"));
+                break;
+            case JUICE_GLASS:
+                this.texture = new Texture(Gdx.files.internal("copo_cheio.png"));
+                break;
+            case EMPTY_GLASS:
+                this.texture = new Texture(Gdx.files.internal("copo_vazio.png"));
+                break;
+        }
+
+        this.bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
+    }
+
+    public void update(float delta, float moveDirection, float speed) {
+        x += moveDirection * speed * 0.9f * delta;
+        bounds.setPosition(x, y);
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(texture, x, y, texture.getWidth(), texture.getHeight());
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+
+    public boolean isOutOfBounds(float limit) {
+        return x < -limit; // Remove o objeto quando ultrapassa `VIRTUAL_WIDTH * 2`
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+}
